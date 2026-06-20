b2efdc4aed439a761eda33da073e5f209d9caa54
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2026-06-20 17:15:17 +0700

    feat: add ReactMemo page to demonstrate component optimization with React.memo

--- src/pages/ReactMemo.jsx
+++ src/pages/ReactMemo.jsx
@@ -1,6 +1,24 @@
+import React, { useState } from "react"
+
+const ChildComponent = React.memo(({count}) => {
+    console.log('ChildComponent rendered')
+    return <h2>Count: {count}</h2>
+})
+
+
 function ReactMemo() {
+  const [count, setCount] = useState(0)
+  const [otherState, setOtherState] = useState(0)
+
   return (
-    <div>ReactMemo</div>
+    <div>
+        <ChildComponent count={count}/>
+
+        <button onClick={()=>setCount(count+1)}>Add Count</button>
+        <button onClick={()=>setOtherState(otherState+1)}>Change Other State {otherState}</button>
+
+        <p>Other State: {otherState}</p>
+    </div>
   )
 }
 

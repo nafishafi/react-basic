746581d0a22934f036e9a8441eeb368be69b5a9d
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2026-06-20 10:17:16 +0700

    feat: add ClassComponent with state management and ChildComponent integration

--- src/components/ClassComponent.jsx
+++ src/components/ClassComponent.jsx
@@ -5,15 +5,32 @@ class ClassComponent extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      count : 1
+      count : 1,
+      input: ''
     }
+
+    this.addNumber = this.addNumber.bind(this)
+  }
+
+  addNumber(number){
+    this.setState((prevState=>({count:prevState.count + number})))
   }
 
   render () {
     return <div>
-        <p>Count: {this.state.count}</p>
-        <button onClick={() => this.setState((prevState=>({count:prevState.count+1})))}>Add +1</button>
+        <button onClick={() => this.addNumber(1)}>Add +1</button>
+        <button onClick={() => this.addNumber(2)}>Add +2</button>
         <ChildComponent count={this.state.count}/>
+
+        <input 
+            type="text" 
+            value={this.state.input}
+            onChange={(e)=>{
+                this.setState({input: e.target.value})
+            }}
+        />
+        <p>Yang ditulisakan: {this.state.input}</p>
+            
     </div>
   }
 }

c9b62d21426bb67b8bf5d6527618e31878dd64b1
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2026-06-20 14:32:21 +0700

    feat: implement theme toggle context and setup React Router navigation structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
 import Detail from './pages/Detail'
+import { useContext } from 'react'
+import { ThemeContext } from './contexts/ThemeContext'
 // import FunctionalComponent from './components/FunctionalComponent'
 // import ClassComponent from './components/ClassComponent'
 // import HooksComponent from './components/HooksComponent'
@@ -22,18 +24,28 @@ function App() {
   //   </>
   // )
 
+  const themeContext = useContext(ThemeContext)
+  console.log(themeContext)
+
+  const changeTheme = () => {
+    themeContext.setTheme(themeContext.theme === 'dark' ? 'light' : 'dark')
+  }
+  
   return (
     <BrowserRouter>
-      <div>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/about'}>About</Link>
-        <Link to={'/detail/1'}>Detail</Link>
+      <div className={themeContext.theme}>
+        <div>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/about'}>About</Link>
+          <Link to={'/detail/1'}>Detail</Link>
+        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/detail/:id' element={<Detail />} />
+        </Routes>
+        <button onClick={changeTheme}>Change Theme to {themeContext.theme === 'light' ? 'dark' : 'light'}</button>
       </div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/detail/:id' element={<Detail />} />
-      </Routes>
     </BrowserRouter>
   )
 

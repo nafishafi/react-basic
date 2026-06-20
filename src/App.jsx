1dd5e6bf2308b4194187da6d51b0ed758d18708a
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2026-06-20 13:49:03 +0700

    feat: implement basic routing with Home, About, and Detail pages in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Detail from './pages/Detail'
 // import FunctionalComponent from './components/FunctionalComponent'
 // import ClassComponent from './components/ClassComponent'
-import HooksComponent from './components/HooksComponent'
+// import HooksComponent from './components/HooksComponent'
 
 
 
 function App() {
 
-  return (
-    <>
-      {/* Hello World!
-      <FunctionalComponent />
-      <FunctionalComponent />
-      <ClassComponent /> */}
+  // return (
+  //   <>
+  //     {/* Hello World!
+  //     <FunctionalComponent />
+  //     <FunctionalComponent />
+  //     <ClassComponent /> */}
 
-      <HooksComponent />
-    </>
+  //     {/* <HooksComponent /> */}
+  //   </>
+  // )
+
+  return (
+    <BrowserRouter>
+      <div>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/about'}>About</Link>
+        <Link to={'/detail/1'}>Detail</Link>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/detail/:id' element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
   )
+
+
 }
 
 export default App
---
+++ src/pages/Detail.jsx
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom"
+
+function Detail() {
+  const {id} = useParams()
+
+  return (
+    <div>Detail : {id}</div>
+  )
+}
+
+export default Detail
